c5293ab58552bbf28376a4c5a25a00281ae169b0
Kaaldev <85872662+Kaaldev@users.noreply.github.com>
2022-07-20 07:32:35 +0530

    Update audio.js

--- routes/audio.js
+++ routes/audio.js
@@ -13,7 +13,9 @@ router.get('/', async (req, res) => {
         let info = await ytdl.getInfo(url);
         console.log(info.videoDetails.title);
         const title = slugify(info.videoDetails.title, {
-            replacement: '-'new''official''hindi'' punjabi'',
+            replacement: '-',
+            replacement:new' ',
+          
             remove: /[*+~.()'"!:@]/g,
             lower: false,
             strict: false
